627250a0222f5712baa109c99d3fe3070ab8e409
DeeaMG <55800409+DeeaMG@users.noreply.github.com>
2020-03-09 14:54:59 +0200

    Update RandomWords.cpp

--- Hangman/RandomWords.cpp
+++ Hangman/RandomWords.cpp
@@ -9,18 +9,16 @@ RandomWords::RandomWords()
 	WordList = { "acres", "adult", "advice", "arrangement", "attempt", "August", "Autumn" };
 }
 
-RandomWords::~RandomWords()
-{
-}
-
 std::string RandomWords::RandomWord()
 {
 	int rand_i;
-	
 	std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
 	std::uniform_int_distribution<int> randNum(0, WordList.size() - 1);
-	
 	rand_i = randNum(mersenne);
 	
 	return WordList[rand_i];
+}
+
+RandomWords::~RandomWords()
+{
 }
